88aa3c781e2805d30a6536bd8cd5dd3c4ac8c6f3
criogenic <criogenic@gmail.com>
2016-01-26 17:19:58 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -280,18 +280,22 @@ $( document ).ready(function() {
 $( window ).load(function() {
   console.log( "window loaded" );
       // Set initial values
-  $("[name=display-status]").val( getQueryParam("status" , 0) );
-  $("[name=display-status]").selectmenu("refresh");
+  //$("[name=display-status]").val( getQueryParam("status" , 0) );
+  $("[name=display-status]").selectedIndex = getQueryParam("status" , 0);
   console.log( "Get status " + getQueryParam("status" , 0));
-  $("[name=display-bg]").val( getQueryParam("background" , 0) );
-  $("[name=display-bg]").selectmenu("refresh");
+  
+  //$("[name=display-bg]").val( getQueryParam("background" , 0) );
+  $("[name=display-bg]").selectedIndex = getQueryParam("background" , 0);
   console.log( "Get background " + getQueryParam("background" , 0));
-  $("[name=language]").val( getQueryParam( "language" , 0) );
-  $("[name=language]").selectmenu("refresh");
+  
+  //$("[name=language]").val( getQueryParam( "language" , 0) );
+  $("[name=language]").selectedIndex = getQueryParam("language" , 0);
   console.log( "Get language " + getQueryParam("language" , 0));
-  $("[name=format]").val( getQueryParam( "format" , 0 ) );
-  $("[name=format]").selectmenu("refresh");
+  
+  //$("[name=format]").val( getQueryParam( "format" , 0 ) );
+  $("[name=format]").selectedIndex = getQueryParam("format" , 0);
   console.log( "Get format " + getQueryParam("format" , 0));
+  
   //$("[name=backgroundcolor]").val( $.params( "backgroundcolor" ) );
   //$("[name=textcolor]").val( $.params( "textcolor" ) );
   //$("#bkg-color").spectrum("set", GColor.toStrHex( getQueryParam( "backgroundcolor" , 0 ) ) );
